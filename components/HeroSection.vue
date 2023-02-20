5bf24a630f14c621e73cceb84da0edfae38736b0
Ivan <ivandanilov282@gmail.com>
2023-02-20 01:35:08 +0100

    Replaced discount image

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -2,9 +2,12 @@
   <div class="container">
     <div class="row">
       <div class="col col-12 col-md-6 mt-5 pt-8 position-relative">
-        <span style="background-color: #1b1b22;" class="p-2 rounded-3 pb-3">
-          <img src="../assets/images/hero-section/discount.svg" alt="Discount"/>
-        </span>
+        <div class="discount-bg-color p-2 rounded-3 w-65 fs-7 d-flex flex-row align-items-center">
+          <img src="../assets/images/hero-section/discount-icon.svg" alt="Discount icon" class="ms-1"/>
+          <span class="text-white ms-2">
+            20% <span class="text-primary">DISCOUNT FOR</span> 1 MONTH <span class="text-primary">ACCOUNT</span>
+          </span>
+        </div>
         <h1 class="text-white pt-3 pb-5">
           The Next<br/><span class="custom-font-gradient">Generation</span><br/> Payment Method.
         </h1>
@@ -14,7 +17,8 @@
         </p>
         <div class="position-absolute w-65 h-75 top-0 start-minus-60 left-gradient"/>
       </div>
-      <div class="col col-12 col-md-6 p-0 py-5 d-flex flex-row justify-content-end align-items-center position-relative">
+      <div
+        class="col col-12 col-md-6 p-0 py-5 d-flex flex-row justify-content-end align-items-center position-relative">
         <div class="ratio ratio-1x1">
           <img src="../assets/images/hero-section/hand.svg" alt="Hand"/>
         </div>
@@ -79,5 +83,13 @@ p {
   background: linear-gradient(180deg, rgba(188, 165, 255, 0) 0%, #214D76 100%);
   border: 1px solid #000000;
   filter: blur(130.5px);
+}
+
+.fs-7 {
+  font-size: 18px;
+}
+
+.discount-bg-color {
+  background-color: #1b1b22;
 }
 </style>
