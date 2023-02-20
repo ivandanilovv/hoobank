32f2130f224db8bf7f31b9e234cb17077acf75f6
Ivan <ivandanilov282@gmail.com>
2023-02-20 16:11:17 +0100

    Added number of users section

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -25,18 +25,97 @@
         <div class="position-absolute w-80 h-60 top-0 end-10 right-gradient"/>
       </div>
     </div>
+    <div class="row py-5 px-3">
+      <div v-for="(item, i) in usersItems"
+           class="col col-12 col-lg-4 d-flex flex-row align-items-center p-0"
+           :class="item.class">
+        <span class="custom-border-left d-none" :class="{'d-lg-block' : i === 1}"/>
+        <h2 class="text-white">
+          {{ item.number }}
+        </h2>
+        <div class="custom-font-gradient fs-5 fw-normal ms-3">
+          {{ item.text }}
+        </div>
+        <span class="custom-border-right d-none" :class="{'d-lg-block' : i === 1}"/>
+      </div>
+    </div>
+<!--    <div class="py-5 px-3 d-flex flex-row align-items-center justify-content-evenly">-->
+<!--      <div class="d-flex flex-row align-items-center">-->
+<!--        <h2 class="text-white">-->
+<!--          3800+-->
+<!--        </h2>-->
+<!--        <div class="custom-font-gradient fs-5 fw-normal ms-3">-->
+<!--          USER ACTIVE-->
+<!--        </div>-->
+<!--      </div>-->
+<!--      <div class="d-flex flex-row align-items-center">-->
+<!--        <span class="custom-border-left"/>-->
+<!--        <h2 class="text-white">-->
+<!--          230+-->
+<!--        </h2>-->
+<!--        <div class="custom-font-gradient fs-5 fw-normal ms-3">-->
+<!--          TRUSTED BY COMPANY-->
+<!--        </div>-->
+<!--      </div>-->
+<!--      <span class="custom-border-right"/>-->
+<!--      <div class="d-flex flex-row align-items-center">-->
+<!--        <h2 class="text-white">-->
+<!--          $230M+-->
+<!--        </h2>-->
+<!--        <div class="custom-font-gradient fs-5 fw-normal ms-3">-->
+<!--          TRANSACTION-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
   </div>
 </template>
 
 <script>
 export default {
-  name: "HeroSection"
+  name: "HeroSection",
+  data() {
+    return {
+      usersItems: [
+        {
+          number: '3800+',
+          text: 'USER ACTIVE',
+          class: 'justify-content-center justify-content-sm-start'
+        },
+        {
+          number: '230+',
+          text: 'TRUSTED BY COMPANY',
+          class: 'justify-content-center justify-content-lg-between'
+        },
+        {
+          number: '$230M+',
+          text: 'TRANSACTION',
+          class: 'justify-content-center justify-content-sm-end'
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
+
+.custom-border-right {
+  border-left: rgba(255, 255, 255, 0.6) 1.70386px solid;
+  height: 30% !important;
+}
+
+.custom-border-left {
+  border-left: rgba(255, 255, 255, 0.6) 1.70386px solid;
+  height: 30% !important;
+}
+
 h1 {
   font-size: 72px;
+  font-weight: 600;
+}
+
+h2 {
+  font-size: 40px;
   font-weight: 600;
 }
 
