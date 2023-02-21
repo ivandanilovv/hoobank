8ccc7945dbcf067c4f53bd92eacd0edea684af0f
Ivan <ivandanilov282@gmail.com>
2023-02-21 23:34:02 +0100

    Made the section responsive

--- components/BillingAndInvoicing.vue
+++ components/BillingAndInvoicing.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="container py-5">
+  <div class="container py-3">
     <div class="row">
       <div class="col col-12 col-md-6">
         <div class="ratio ratio-1x1">
           <img src="../assets/images/billing-and-invoicing/paypal.svg" alt="Paypal"/>
         </div>
       </div>
-      <div class="col col-12 col-md-5 offset-md-1 d-flex flex-column align-items-start justify-content-center">
+      <div class="col col-12 col-md-6 col-xl-5 offset-xl-1 text-center text-lg-start
+      d-flex flex-column align-items-center align-items-lg-start justify-content-center">
         <h1 class="text-white">
           Easily control your billing & invoicing.
         </h1>
         <p class="text-primary m-0 my-4">
-          Elit enim sed massa etiam. Mauris eu adipiscing<br/>
-          ultrices ametodio aenean neque. Fusce ipsum orci<br/>
+          Elit enim sed massa etiam. Mauris eu adipiscing<br class="d-none d-sm-block d-md-none d-xl-block"/>
+          ultrices ametodio aenean neque. Fusce ipsum orci<br class="d-none d-sm-block d-md-none d-xl-block"/>
           rhoncus aliporttitor integer platea placerat.
         </p>
         <div>
