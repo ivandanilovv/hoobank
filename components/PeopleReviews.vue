14f0ebc3eaf9ae9083df8318cfce704d05c5dcd9
Ivan <ivandanilov282@gmail.com>
2023-02-22 01:58:30 +0100

    Created 'What people are saying about us' component

---
+++ components/PeopleReviews.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <div class="row text-center text-lg-start">
+      <div class="col col-12 col-md-6">
+        <h1 class="text-white">
+          What people are saying about us
+        </h1>
+      </div>
+      <div class="col col-12 col-md-6 d-flex flex-column align-items-center justify-content-center">
+        <p class="text-primary m-0 mt-3 mb-2">
+          Everything you need to accept card payments and grow your business anywhere on the planet.
+        </p>
+      </div>
+    </div>
+    <div class="row py-3">
+      <div v-for="cardDetails in reviewCards" class="col col-12 col-md-6 col-lg-4 mt-4">
+        <review-card :card-details="cardDetails"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ReviewCard from "@/components/ReviewCard";
+
+export default {
+  name: "PeopleReviews",
+  components: {ReviewCard},
+  data() {
+    return {
+      reviewCards: [
+        {
+          img: require('../assets/images/people-reviews/herman.svg'),
+          alt: 'Herman Jensen image',
+          name: 'Herman Jensen',
+          description: 'Founder & Leader',
+          text: 'Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.',
+        },
+        {
+          img: require('../assets/images/people-reviews/steve.svg'),
+          alt: 'Steve Mark image',
+          name: 'Steve Mark',
+          description: 'Founder & Leader',
+          text: 'Money makes your life easier. If you\'re lucky to have it, you\'re lucky.',
+        },
+        {
+          img: require('../assets/images/people-reviews/kenn.svg'),
+          alt: 'Kenn Gallagher image',
+          name: 'Kenn Gallagher',
+          description: 'Founder & Leader',
+          text: 'It is usually people in the money business, finance, and international trade that are really rich.',
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 48px;
+  font-weight: 600;
+}
+
+p {
+  font-size: 18px;
+  font-weight: 400;
+}
+</style>
---
+++ components/ReviewCard.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="p-5 rounded-4 h-100 d-flex flex-column justify-content-between card-background-gradient">
+    <img src="../assets/images/people-reviews/quotation-mark.svg" alt="Quotation mark" width="43" height="28"/>
+    <p class="text-primary py-4">
+      {{ cardDetails.text }}
+    </p>
+    <div class="row">
+      <div class="col col-3 d-flex flex-column justify-content-center">
+        <div class="ratio ratio-1x1">
+        <img :src="cardDetails.img" :alt="cardDetails.alt"/>
+        </div>
+      </div>
+      <div class="col col-9 d-flex flex-column justify-content-center">
+        <h5 class="text-white m-0">
+          {{ cardDetails.name }}
+        </h5>
+        <p class="text-primary m-0">
+          {{cardDetails.description}}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ReviewCard",
+  props: {
+    cardDetails: Object,
+  }
+}
+</script>
+
+<style scoped>
+h5 {
+  font-size: 20px;
+  font-weight: 400;
+}
+
+p {
+  font-size: 18px;
+  font-weight: 400;
+}
+
+.card-background-gradient {
+
+  /* ff 3.6+ */
+  background:-moz-linear-gradient(180deg, rgba(79, 77, 86, 1) 0%, rgba(20, 16, 29, 1) 100%);
+
+  /* safari 5.1+,chrome 10+ */
+  background:-webkit-linear-gradient(180deg, rgba(79, 77, 86, 1) 0%, rgba(20, 16, 29, 1) 100%);
+
+  /* opera 11.10+ */
+  background:-o-linear-gradient(180deg, rgba(79, 77, 86, 1) 0%, rgba(20, 16, 29, 1) 100%);
+
+  /* ie 6-9 */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#14101D', endColorstr='#4F4D56', GradientType=1 );
+
+  /* ie 10+ */
+  background:-ms-linear-gradient(180deg, rgba(79, 77, 86, 1) 0%, rgba(20, 16, 29, 1) 100%);
+
+  /* global 94%+ browsers support */
+  background:linear-gradient(180deg, rgba(79, 77, 86, 1) 0%, rgba(20, 16, 29, 1) 100%);
+}
+</style>
