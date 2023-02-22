e9b42ed389d16304e96d8e7635107a418dfa5800
Ivan <ivandanilov282@gmail.com>
2023-02-22 02:32:33 +0100

    Added links to the images

--- components/Clients.vue
+++ components/Clients.vue
@@ -3,7 +3,9 @@
     <div class="row">
       <div class="col col-12 col-sm-6 col-lg-3 d-flex flex-row align-items-center justify-content-center mt-sm-2"
            v-for="(image, i) in clientsImages" :class="{'mt-4' : i !== 0}">
+        <a :href="image.link" target="_blank">
           <img :src="image.src" :alt="image.alt"/>
+        </a>
       </div>
     </div>
   </div>
@@ -17,19 +19,23 @@ export default {
       clientsImages: [
         {
           src: require('../assets/images/clients/airbnb.svg'),
-          alt: 'Airbnb Logo'
+          alt: 'Airbnb Logo',
+          link: 'https://www.airbnb.com/',
         },
         {
           src: require('../assets/images/clients/binance.svg'),
-          alt: 'Binance Logo'
+          alt: 'Binance Logo',
+          link: 'https://www.binance.com/en'
         },
         {
           src: require('../assets/images/clients/coinbase.svg'),
-          alt: 'Coinbase Logo'
+          alt: 'Coinbase Logo',
+          link: 'https://www.coinbase.com/'
         },
         {
           src: require('../assets/images/clients/dropbox.svg'),
-          alt: 'Dropbox Logo'
+          alt: 'Dropbox Logo',
+          link: 'https://www.dropbox.com/'
         },
       ]
     }
